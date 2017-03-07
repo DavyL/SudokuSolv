5a78a8eb6e7ec94db24ee43341ee689400f6722a
DavyL <cr4zypxl@gmail.com>
2017-03-07 11:22:46 +0100

    Some new basic functions

--- src/main.py
+++ src/main.py
@@ -1,5 +1,16 @@
 def readSudoku(Filename):
-	print("readSudoku(Filename")
+	l=[]
+	with open(Filename) as f:
+		for line in f:
+			for i in range(9):
+				l.append((int(line[i],[])))
 def printSudoku(s):
-	print("printSudoku(s)")
-
+	k = 0
+	for i in s:
+		k = k+1
+		if k == 9:
+			print(i[0])
+		else:
+			print(i[0], end = "")
+def m2a(x, y):
+	return x*9 + y
