41de55cdc60cde5ae330d8ed1471a0e8881e8066
DavyL <cr4zypxl@gmail.com>
2017-03-15 23:16:08 +0100

    The program now solves basic cases when there is only one possibility for each line or column, added several solving functions and utility functions : updateLineCand(), swapSingleCand(), swapSignleCandLine(), cleanColumns(), cleanLine(), cleanTtrivialCand(), cleandSGrid(). That's it for today, EOD

--- src/main.py
+++ src/main.py
@@ -1,11 +1,4 @@
-def cl_readSudoku(Filename):
-	l=[]
-	with open(Filename) as f:
-		for line in f:
-			for i in range(9):
-				l.append((int(line[i]),[]))
-	f.close()
-	return l
+
 
 #Read a file containing a sudoku
 #File shall be a python list
@@ -15,6 +8,7 @@ def readSudoku(filename):
     fd.close()
     return eval(l[0])
 
+#Creates a standard grid cointaining no elements
 def newEmptyGrid():
 	l = []
 	for i in range(9):
@@ -23,14 +17,80 @@ def newEmptyGrid():
 			l[i].append((0,[1,2,3,4,5,6,7,8,9]))
 	return l
 
-def cl_printSudoku(s):
-	k = 0
-	for i in s:
-		k = k+1
-		if k == 9:
-			print(i[0])
-		else:
-			print(i[0], end = "")
+#Takes a grid containing only list containing line elements
+#Returns a grid containing each elements from input and all of the possible candidates as tuple
+def grid2SGrid(grid):
+    for numLine in range(len(grid)):
+        for elem in range(9):
+            grid[numLine][elem] = (grid[numLine][elem], [i for i in range(1,9)]) 
+    cleanSGrid(grid)
+    return grid
+
+#Checks for non zero values of a standard grid
+#When found their second members switch to an empty list
+def cleanSGrid(SGrid):
+    
+    cleanTrivialCand(SGrid)    
+    cleanLine(SGrid)
+    cleanColumns(SGrid) 
+    cleanLine(SGrid)
+    return SGrid
+
+#If there is already a value removes candidates
+def cleanTrivialCand(SGrid):
+    for lineNum in range(9):            #Cleans trivial values
+        for elem in range(9):
+            if SGrid[lineNum][elem][0] != 0:
+                SGrid[lineNum][elem] = (SGrid[lineNum][elem][0], [])
+    return SGrid
+
+#Removes from candidates values that are already in the same line
+#Parses a grid
+def cleanLine(SGrid):
+    for i in range(9):                  #Cleans line
+        SGrid[i] = updateLineCand(SGrid[i])
+        for elemNum in range(len(SGrid[i])):
+            if SGrid[i][elemNum][0] == 0:
+                if len(SGrid[i][elemNum][1]) == 1:
+                    SGrid[i][elemNum] = (SGrid[i][elemNum][1][0], [])
+    return SGrid
+
+#Removes from candidates values that are already in the same columns
+#Parses a grid
+def cleanColumns(SGrid):    
+    SGrid = retColumns(SGrid)
+    cleanLine(SGrid)
+    SGrid = retColumns(SGrid)
+   
+    return SGrid
+
+#If there's only one single candidate, then sets it as a value
+#Parses the whole grid
+def swapSingleCand(SGrid):
+    for lineNum in range(9):
+        SGrid[lineNum] = swapSingleCandLine(SGrid[lineNum])
+    return SGrid
+
+
+#If there's only one single candidate, then sets it as a value
+#Parses only a line
+def swapSingleCandLine(SLine):
+    for elemNum in range(9):
+        if len(SLine[elemNum][1]) == 1:
+            SLine[elemNum] = (SLine[elemNum][1][0], [])
+
+    return SLine
+
+#Updates the candidates of a line
+def updateLineCand(SLine):
+    for elemNum in range(9):
+        if SLine[elemNum][0] == 0:
+            SLineLen = len(SLine[elemNum][1])
+            for candNum in range(SLineLen):
+                cand = SLine[elemNum][1][SLineLen -1 - candNum]
+                if (cand, []) in SLine:
+                    SLine[elemNum][1].remove(cand)
+    return SLine
 
 #Prints the Sudoku
 def printSudoku(grid):
@@ -80,7 +140,40 @@ def checkLine(line):
 
     return list(set(issue))
         
-def computeCandidates(s):
+grid =	readSudoku("../grid/grid1.gr")
+grid[2][2] = 0
+grid[2][3] = 0 
+printSudoku(grid)
+if (checkSudoku(grid) != ([],[])):
+        print("OH DEAR, WE ARE IN TROUBLE")
+SGrid = grid2SGrid(grid)
+printSudoku(SGrid)
+
+
+##################################################################################################
+##################################################################################################
+##################################################################################################
+##################################################################################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#All of functions below have been written by teacher
+#They are here just in case they might turn out to be useful
+#They will probably be removed soon
+
+def cl_computeCandidates(s):
 	for i in range(9):
 		for j in range(9):
 			e = s[m2a(i,j)]
@@ -93,10 +186,24 @@ def computeCandidates(s):
 					if el[0] != 0 and el[0] in e[1]:
 						e[1].remove(el[0])
 
-def m2a(x, y):
+def cl_m2a(x, y):
 	return x*9 + y
 
-grid =	readSudoku("../grid/grid1.gr")
-printSudoku(grid)
-if (checkSudoku(grid) != ([],[])):
-        print("OH DEAR, WE ARE IN TROUBLE")
+def cl_readSudoku(Filename):
+	l=[]
+	with open(Filename) as f:
+		for line in f:
+			for i in range(9):
+				l.append((int(line[i]),[]))
+	f.close()
+	return l
+
+def cl_printSudoku(s):
+	k = 0
+	for i in s:
+		k = k+1
+		if k == 9:
+			print(i[0])
+		else:
+			print(i[0], end = "")
+
