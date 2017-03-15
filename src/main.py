d68c69b2e1fdb915b9615b9a15a2bc115b697f08
Leo Davy <cr4zypxl@gmail.com>
2017-03-15 20:39:24 +0100

    Added several utility functions : readSudoku(), printSudoku(), retColumns(), checkSudoku(), checkLine()

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-def readSudoku(Filename):
+def cl_readSudoku(Filename):
 	l=[]
 	with open(Filename) as f:
 		for line in f:
@@ -6,6 +6,14 @@ def readSudoku(Filename):
 				l.append((int(line[i]),[]))
 	f.close()
 	return l
+
+#Read a file containing a sudoku
+#File shall be a python list
+def readSudoku(filename):
+    fd = open(filename)
+    l = fd.readlines()
+    fd.close()
+    return eval(l[0])
 
 def newEmptyGrid():
 	l = []
@@ -15,7 +23,7 @@ def newEmptyGrid():
 			l[i].append((0,[1,2,3,4,5,6,7,8,9]))
 	return l
 
-def printSudoku(s):
+def cl_printSudoku(s):
 	k = 0
 	for i in s:
 		k = k+1
@@ -24,6 +32,54 @@ def printSudoku(s):
 		else:
 			print(i[0], end = "")
 
+#Prints the Sudoku
+def printSudoku(grid):
+    for line in range(len(grid)):
+        print(grid[line])
+
+
+#Returns a list of all the columns of the sudoku
+def retColumns(grid):
+    columns = []
+    for i in range(9):
+        columns.append([])
+    for line in grid:
+        for i in range(9):
+            columns[i].append(line[i])
+    return columns
+
+
+#Checks if the respects the rules
+#Checks lines then columns
+#return the lists containing issues inside lines then inside columns
+#If the grid contains no zero value then each list are the same 
+def checkSudoku(grid):
+    issueL = []
+    for line in grid:
+        if checkLine(line) != []:
+            issueL.append(checkLine(line))
+    issueC = []
+    for columns in retColumns(grid):
+        if checkLine(columns) != []:
+            issueC.append(checkLine(columns))
+    return issueL, issueC
+
+    
+
+#Checks if each element of a line appears only once
+#Returns a list containing each value that appears more than once
+def checkLine(line):
+    issue = []
+    for j in range(1,9):
+        count = 0
+        for i in line:
+            if i == j:
+                count +=1
+            if(count > 1):
+                issue.append(j)
+
+    return list(set(issue))
+        
 def computeCandidates(s):
 	for i in range(9):
 		for j in range(9):
@@ -40,17 +96,7 @@ def computeCandidates(s):
 def m2a(x, y):
 	return x*9 + y
 
-def fillGrid(grid):	
-	grid[][1] = (8, None)
-	grid[][2] = (7, None)
-	grid[][6] = (9, None)
-	grid[][7] = (9, None)
-	grid[1][2] = (9, None)
-	grid[1][2] = (9, None)
-	grid[1][2] = (9, None)
-	grid[1][2] = (9, None)
-	grid[1][2] = (9, None)
-	grid[1][2] = (9, None)
-grid = newEmptyGrid()
-grid[1][2] = (9, None)
-print(grid)
+grid =	readSudoku("../grid/grid1.gr")
+printSudoku(grid)
+if (checkSudoku(grid) != ([],[])):
+        print("OH DEAR, WE ARE IN TROUBLE")
